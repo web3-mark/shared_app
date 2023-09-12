52eac5dc074698ca43e541286aade41af79b52a4
mmmllltest1 <mmmllltest57@gmail.com>
2023-09-12 23:36:25 +0800

    feat: change api path

--- lib/event_bus.dart
+++ lib/event_bus.dart
@@ -1,7 +1,53 @@
-import 'package:event_bus_plus/event_bus_plus.dart';
+//订阅者回调签名
+typedef EventCallback = void Function(Map? arg);
 
-// Initialize the Service Bus
-// ignore: non_constant_identifier_names
-IEventBus event_bus = EventBus();
+enum Events {
+  ChangWallet,
+  Transfer,
+  Unlock,
+}
 
-class LoginEvent {}
+class EventBus {
+  //私有构造函数
+  EventBus._internal();
+
+  //保存单例
+  static final EventBus _singleton = EventBus._internal();
+
+  //工厂构造函数
+  factory EventBus() => _singleton;
+
+  //保存事件订阅者队列，key:事件名(id)，value: 对应事件的订阅者队列
+  final _emap = <Object, List<EventCallback>?>{};
+
+  //添加订阅者
+  void on(Events eventName, EventCallback f) {
+    _emap[eventName] ??= <EventCallback>[];
+    _emap[eventName]!.add(f);
+  }
+
+  //移除订阅者
+  void off(Events eventName, [EventCallback? f]) {
+    var list = _emap[eventName];
+    if (list == null) return;
+    if (f == null) {
+      _emap[eventName] = null;
+    } else {
+      list.remove(f);
+    }
+  }
+
+  //触发事件，事件触发后该事件所有订阅者会被调用
+  void emit(Events eventName, [arg]) {
+    var list = _emap[eventName];
+    if (list == null) return;
+    int len = list.length - 1;
+    //反向遍历，防止订阅者在回调中移除自身带来的下标错位
+    for (var i = len; i > -1; --i) {
+      list[i](arg);
+    }
+  }
+}
+
+//定义一个top-level（全局）变量，页面引入该文件后可以直接使用bus
+final eventbus = EventBus();
--- lib/shared_app.dart
+++ lib/shared_app.dart
@@ -1,1 +1,5 @@
 library shared_app;
+
+export "./app_controller.dart";
+export './constants.dart';
+export './event_bus.dart';
