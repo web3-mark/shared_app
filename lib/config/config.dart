db238ca2501222674c53c25ac46931845e9eb953
mmmllltest1 <mmmllltest57@gmail.com>
2023-09-23 17:02:33 +0800

    feat: update url

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -5,5 +5,5 @@ class AppConfig {
   static bool get isDev => appEnv == 'test';
 
   static String get app =>
-      isDev ? "http://testton.fbixfbi.com" : "https://api.itoncoin.com";
+      isDev ? "http://testgbox.fbixfbi.com" : "https://api.gbox.com";
 }
---
+++ lib/skins/theme.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/rendering.dart';
+
+class ThemeConfig {
+  /// block button 
+  static BoxDecoration button = BoxDecoration();
+}
