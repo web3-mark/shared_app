eb3bb247820a6f9afb1a8fee63233da3e7312852
mmmllleth <mmmlll@gmail.com>
2023-11-12 20:44:07 +0800

    feat: add radius prop

--- lib/widgets/cornerd_box.dart
+++ lib/widgets/cornerd_box.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:shared_app/skins/base.dart';
 
 class CorneredBox extends StatelessWidget {
   final Widget child;
   final double? height;
+  final double radius;
   final double padding;
   const CorneredBox({
     Key? key,
     required this.child,
     this.height,
+    this.radius = 30,
     this.padding = 16,
   }) : super(key: key);
 
@@ -17,9 +18,9 @@ class CorneredBox extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: height,
-      constraints: height == null ? BoxConstraints.expand() : null,
+      constraints: height == null ? const BoxConstraints.expand() : null,
       padding: EdgeInsets.symmetric(horizontal: padding),
-      decoration: box2,
+      decoration: box2.copyWith(borderRadius: BorderRadius.circular(radius)),
       child: SingleChildScrollView(
         child: child,
       ),
